d04935ce3300cd1babde0745c4f0ef37bb1111a1
Allen, John F <john.allen@nist.gov>
2015-09-08 16:25:31 -0400

    MAJOR: initial commit
    This is the initial commit of the fully working application.

---
+++ config/environment.js
@@ -0,0 +1,53 @@
+/* jshint node: true */
+
+module.exports = function(environment) {
+  var ENV = {
+    modulePrefix: 'embertodocli',
+    contentSecurityPolicy: {
+      'img-src': "'self' http://fpoimg.com",
+      'script-src': "'self' 'unsafe-inline' *",
+      'frame-src' : "'self' 'unsafe-inline' *",
+      'connect-src' : "'self' 'unsafe-inline' *"
+    },
+    environment: environment,
+    baseURL: '/',
+    locationType: 'auto',
+    EmberENV: {
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. 'with-controller': true
+      }
+    },
+
+    APP: {
+      // Here you can pass flags/options to your application instance
+      // when it is created
+    }
+  };
+
+  if (environment === 'development') {
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
+
+  if (environment === 'test') {
+    // Testem prefers this...
+    ENV.baseURL = '/';
+    ENV.locationType = 'none';
+
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+    ENV.APP.rootElement = '#ember-testing';
+  }
+
+  if (environment === 'production') {
+
+  }
+
+  return ENV;
+};
